e58dd405597243c8c753346082083f015379893b
Diogo Queiroz <diogo.qds97@gmail.com>
2020-10-28 18:38:59 -0300

    test: ensure RequiredFieldValidation returns error and null in case validation fails and succeeds

---
+++ src/presentation/helpers/validators/compare-fields-validation.spec.ts
@@ -0,0 +1,26 @@
+import { InvalidParamError } from '../../errors'
+import { CompareFieldsValidation } from './compare-fields-validation'
+
+const makeSut = (): CompareFieldsValidation => {
+  return new CompareFieldsValidation('field', 'fieldToCompare')
+}
+
+describe('RequiredField Validation', () => {
+  test('should return a InvalidParamError if validation fails', () => {
+    const sut = makeSut()
+    const error = sut.validate({
+      field: 'any_field',
+      fieldToCompare: 'wrong_field'
+    })
+    expect(error).toEqual(new InvalidParamError('fieldToCompare'))
+  })
+
+  test('should return null if validation succeeds', () => {
+    const sut = makeSut()
+    const error = sut.validate({
+      field: 'any_field',
+      fieldToCompare: 'any_field'
+    })
+    expect(error).toBeFalsy()
+  })
+})
