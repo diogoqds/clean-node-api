5b5ed35d4059fe192f20c2c1425913f55e2eb8a3
Diogo Queiroz <diogo.qds97@gmail.com>
2020-11-02 16:13:52 -0300

    refactor: change port

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL ?? 'mongodb://localhost:27017/clean-node-api',
-  port: process.env.PORT ?? 5050,
+  port: process.env.PORT ?? 3000,
   jwtSecret: process.env.JWT_SECRET ?? 'sduiahjoualdasias7113'
 }
