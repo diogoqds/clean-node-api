6f69784e7dfd5a3ba58a5fd3256e429be5785b74
Diogo Queiroz <diogo.qds97@gmail.com>
2020-11-08 10:03:13 -0300

    test: ensure JwtAdapter throw if verify throws

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -69,4 +69,14 @@ describe('Jwt Adapter', () => {
       expect(accessToken).toBe('any_value')
     })
   })
+
+  test('should throw if verify throws', async () => {
+    jest.spyOn(jwt, 'verify').mockImplementationOnce(() => {
+      throw new Error()
+    })
+
+    const { sut } = makeSut()
+    const promise = sut.decrypt(makeValue())
+    await expect(promise).rejects.toThrow()
+  })
 })
